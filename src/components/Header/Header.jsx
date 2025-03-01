de7f64efed9c04c8e848a63ea706302c737dcd55
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-03-02 01:29:45 +0530

    Links added for naviation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import logo from "../../images/logo.jpg";
+import { Link } from "react-router";
 
 const Header = () => {
   return (
     <div>
       <header>
-        <label>DevTinder</label>
+        <h1>
+          <Link to={"/"}>DevTinder</Link>
+        </h1>
+        <Link to={"/login"}>Login</Link>
         <img src={logo} alt="logo" />
       </header>
     </div>
