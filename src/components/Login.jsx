10cca9cb4812df5d937c44506368a2d19f5da9cc
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-03-06 23:21:38 +0530

    login  page UI complted.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 
 const Login = () => {
+
+  const handleUsernameChange = () => {};
+  const handlePwdChange = () => {};
+
   return (
     <div>
       <div class="card">
         <div class="container">
-          <h4>
-            <b>John Doe</b>
-          </h4>
-          <p>Architect & Engineer</p>
+          <h2>
+            <b>Login</b>
+          </h2>
+          <div className="form-sec">
+            <div className="form-group">
+              <label>Username</label>
+              <input
+                type="text"
+                name="username"
+                className="form-control"
+                value={""}
+                onChange={handleUsernameChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="text"
+                name="password"
+                className="form-control"
+                value={""}
+                onChange={handlePwdChange}
+              />
+            </div>
+          </div>
         </div>
         <button>Buy now</button>
       </div>
