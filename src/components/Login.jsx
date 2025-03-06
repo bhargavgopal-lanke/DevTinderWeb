f06bc7b4fe1285ff781ab3e7d890d44d3479116b
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-03-06 23:13:40 +0530

    card Ui added.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
     <div>
-      Login
+      <div class="card">
+        <div class="container">
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <p>Architect & Engineer</p>
+        </div>
+        <button>Buy now</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
